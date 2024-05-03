58e9e978d94abbb72661637118fe7ba1fc99c448
Sindhu <sindhureddyvari3@gmail.com>
2024-05-03 12:03:02 +0530

    level-5

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -1,11 +1,24 @@
 import "./App.css";
-import TaskApp from "./TaskApp";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import HomePage from './pages/HomePage';
+import TaskListPage from './pages/TaskListPage';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/tasks",
+    element: <TaskListPage />,
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
-      <TaskApp />
-    </div>
+     <RouterProvider router={router} />
   );
 }
 export default App;
