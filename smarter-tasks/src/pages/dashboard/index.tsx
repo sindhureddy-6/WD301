4cd4d41e08c5d46de8c2e04600b06b38473bb527
Sindhu <sindhureddyvari3@gmail.com>
2024-05-23 20:40:34 +0530

    modified sigin-in page

--- smarter-tasks/src/pages/dashboard/index.tsx
+++ smarter-tasks/src/pages/dashboard/index.tsx
@@ -1,37 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Dashboard: React.FC = () => {
-const userDataString = localStorage.getItem('userData');
-if (userDataString) {
-    const userData = JSON.parse(userDataString);
+  const [userData, setUserData] = useState<{ name: string; email: string } | null>(null);
 
-    // Assuming userData has properties 'name' and 'email'
-    const userNameElement = document.getElementById('user-name');
-    const userEmailElement = document.getElementById('user-email');
-
-    if (userNameElement && userEmailElement) {
-        userNameElement.textContent = userData.name;
-        userEmailElement.textContent = userData.email;
+  useEffect(() => {
+    const userDataString = localStorage.getItem('userData');
+    if (userDataString) {
+      const userData = JSON.parse(userDataString);
+      setUserData(userData);
     } else {
-        console.error('User info elements not found in the DOM');
+      console.error('No user data found in localStorage');
     }
-} else {
-    console.error('No user data found in localStorage');
-}
-    const handleClick = () => {
-        localStorage.removeItem("authToken");
-        localStorage.removeItem("userData");
-      }
-    return (
+  }, []);
+
+  const handleClick = () => {
+    localStorage.removeItem('authToken');
+    localStorage.removeItem('userData');
+    setUserData(null); 
+  };
+
+  return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
-          <h1 className="text-3xl font-bold text-center text-gray-800 mb-8">Dashboard</h1>
-          <div id="user-info">
-        <p>Name: <span id="user-name"></span></p>
-        <p>Email: <span id="user-email"></span></p>
-          </div>
-          <button onClick={handleClick} id="logout-link" className="bg-gray-700 hover:bg-gray-800 text-white font-semibold py-2  mt-4 rounded">Log-out</button>
+      <div className="text-center">
+        <h1 className="text-3xl font-bold text-center text-gray-800 mb-8">Dashboard</h1>
+        <div id="user-info">
+          <p>Name: <span id="user-name">{userData?.name}</span></p>
+          <p>Email: <span id="user-email">{userData?.email}</span></p>
+        </div>
+        <button
+          onClick={handleClick}
+          id="logout-link"
+          className="bg-gray-700 hover:bg-gray-800 text-white font-semibold py-2 mt-4 rounded"
+        >
+          Log-out
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default Dashboard;
